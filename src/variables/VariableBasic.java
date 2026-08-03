a320e17edbeb2a1d7b50e5a58aebe624ba5980f6
MunjurKadir <Munjurkadir656@gmail.com>
2026-08-03 08:36:52 +0600

    Java Variable Basics

---
+++ src/variables/VariableBasic.java
@@ -0,0 +1,24 @@
+package variables;
+
+public class VariableBasic {
+    public static void main(String[] args){
+        int age = 20;
+        double price = 50.50;
+        float cgpa = 3.88f;
+        char grade = 'A';
+        String name = "Mim Mozahid";
+        boolean isStudent = true;
+        int id;
+        id = 25;
+
+        System.out.println("My name is " + name);
+        System.out.println("My id is " + id);
+        System.out.println(age);
+        System.out.println("I'm "+ age +" years old");
+        System.out.println(price);
+        System.out.println("My CGPA is " + cgpa);
+        System.out.println("I got " + grade);
+        System.out.println("I'm a student? " + isStudent);
+
+    }
+}
