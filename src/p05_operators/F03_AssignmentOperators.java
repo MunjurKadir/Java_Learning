de02f456dc37aa56cb9371cde05352be9e3c1308
MunjurKadir <Munjurkadir656@gmail.com>
2026-08-04 18:21:15 +0600

    Java Assignment Operators

---
+++ src/p05_operators/F03_AssignmentOperators.java
@@ -0,0 +1,20 @@
+package p05_operators;
+
+public class F03_AssignmentOperators {
+    public static void main(String[] args){
+        int savings = 100;
+        savings += 50; // add 50 to savings
+        System.out.println("Total savings: " + savings);
+    }
+}
+//      =    x = 5	  x = 5
+//      +=	 x += 3	  x = x + 3
+//      -=	 x -= 3	  x = x - 3
+//      *=	 x *= 3	  x = x * 3
+//      /=	 x /= 3	  x = x / 3
+//      %=	 x %= 3	  x = x % 3
+//      &=	 x &= 3	  x = x & 3
+//      |=	 x |= 3	  x = x | 3
+//      ^=	 x ^= 3	  x = x ^ 3
+//      >>=	 x >>= 3  x = x >> 3
+//      <<=	 x <<= 3  x = x << 3
