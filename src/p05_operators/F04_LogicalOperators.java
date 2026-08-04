e080d648fe353895d03c23cab4f3441f98c8e591
MunjurKadir <Munjurkadir656@gmail.com>
2026-08-04 18:37:21 +0600

    JavaLogical Operators

---
+++ src/p05_operators/F04_LogicalOperators.java
@@ -0,0 +1,15 @@
+package p05_operators;
+
+public class F04_LogicalOperators {
+    public static void main(String[] args){
+        boolean isLoggedIn = true;
+        boolean isAdmin = false;
+
+        System.out.println("Regular user: " +(isLoggedIn && !isAdmin));
+        System.out.println("Has access: " +(isLoggedIn || isAdmin));
+        System.out.println("Not Logged in: " + !isLoggedIn);
+    }
+}
+//        && 	Logical and 	Returns true if both statements are true
+//        || 	Logical or	    Returns true if one of the statements is true
+//        !	    Logical not	    Reverse the result, returns false if the result is true
