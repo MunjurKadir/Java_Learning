a3dcf6ea96fd4ac9d9786b3c3d0bb2cb871d4f11
MunjurKadir <Munjurkadir656@gmail.com>
2026-08-04 18:16:40 +0600

    Java Comparison Operators

---
+++ src/p05_operators/F02_ComparisonOperators.java
@@ -0,0 +1,17 @@
+package p05_operators;
+
+//The return value of a comparison is either true or false.
+public class F02_ComparisonOperators {
+    public static void main (String[] args){
+        int age = 18;
+
+        System.out.println(age >= 18); // true, old enough to vote
+        System.out.println(age < 18);  // false
+    }
+}
+//  ==	Equal to	x == y
+//  !=	Not equal	x != y
+//  >	Greater than	x > y
+//  <	Less than	x < y
+//  >=	Greater than or equal to	x >= y
+//  <=	Less than or equal to	x <= y
