1ef7b7a484d826d1894ca5e49f38a81a9b8c1701
MunjurKadir <Munjurkadir656@gmail.com>
2026-08-04 18:12:41 +0600

    Java Arithmetic Operators

---
+++ src/p05_operators/F01_ArithmeticOperators.java
@@ -0,0 +1,24 @@
+package p05_operators;
+
+public class F01_ArithmeticOperators {
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 3;
+        System.out.println(x +y); // 13
+        System.out.println(x -y); // 7
+        System.out.println(x *y); // 30
+        System.out.println(x /y); // 3
+        System.out.println(x %y); // 1
+
+        int z = 5;
+        ++z;
+        System.out.println(z); // 6
+        --z;
+        System.out.println(z); // 5
+
+        int m = 5;
+        int n = 3;
+        System.out.println(m > y);
+    }
+}
