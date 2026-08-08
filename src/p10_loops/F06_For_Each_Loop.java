d7cda91800b8a9de67a42e54f5da09ace288fca5
MunjurKadir <Munjurkadir656@gmail.com>
2026-08-08 23:39:27 +0600

    Java For Each Loop

--- src/p10_loops/F06_For_Each_Loop.java
+++ src/p10_loops/F06_For_Each_Loop.java
@@ -6,6 +6,8 @@ public class F06_For_Each_Loop {
         for (String fruit: fruits){
             System.out.println(fruit);
         }
+        // The for-each loop is great when you only need to read elements.
+        // If you want to change the elements later, or keep track of their index, use a regular for loop instead.
         int[] numbers = {2, 4, 6, 8, 7};
         for(int num: numbers){
             System.out.println(num);
